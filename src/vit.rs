2c672c1f49fb404941d7abf6e1663437f2fb796a
neruneruna7 <neruneruna7@gmail.com>
2025-12-16 23:48:39 +0900

    u

--- src/vit.rs
+++ src/vit.rs
@@ -1,6 +1,7 @@
 // CIFAR-10 images are 32x32 pixels
 
 use burn::{
+    module::Param,
     nn::{
         Linear, LinearConfig,
         conv::Conv2d,
@@ -27,6 +28,26 @@ const DIM_FEEDFORWARD: usize = EMBED_VECTOR_LEN;
 const ACTIVATION: &str = "gelu";
 const LAYERS: usize = 12;
 
+fn repeat_interleave<B: Backend, const D: usize, const D2: usize>(
+    tensor: Tensor<B, D>,
+    repeats: usize,
+    dim: usize,
+) -> Tensor<B, D> {
+    // 1. 指定した次元の直後に新しい次元を追加 (Unsqueeze)
+    // 例: shape [3, 5], dim=0 -> [3, 1, 5]
+    let x: Tensor<B, D2> = tensor.unsqueeze_dim(dim + 1);
+
+    // 2. repeat用のシェイプを作成
+    // 元の次元数(D) + 追加した1次元 = D + 1
+    // 基本はすべて1倍で、追加した次元だけ repeats倍にする
+    let mut repeat_shape = vec![1; D + 1];
+    repeat_shape[dim + 1] = repeats;
+
+    // 3. 追加した次元方向にリピートし、その後元の次元とマージ (Flatten)
+    // [3, 1, 5] -> repeat -> [3, 2, 5] -> flatten(0, 1) -> [6, 5]
+    x.repeat(&repeat_shape).flatten(dim, dim + 1)
+}
+
 #[derive(Config, Debug)]
 pub struct VitConfig {
     pub num_layers: usize,
@@ -37,24 +58,37 @@ pub struct VitConfig {
 }
 
 impl VitConfig {
-    pub fn init<B: Backend>(&self) -> Vit<B> {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> Vit<B> {
         let patch_embedding = LinearConfig::new(PATCH_VECTOR_LEN, EMBED_VECTOR_LEN);
+        // Pram from_tensor ではなく， uninitializedを使えとの警告あり
+        // paramIdがなにかわかってないので，ひとまずはこれで
+        let cls: Param<Tensor<B, 3>> =
+            Param::from_tensor(Tensor::zeros(Shape::new([1, 1, EMBED_VECTOR_LEN]), device));
+        let position_embedding: Param<Tensor<B, 3>> = Param::from_tensor(Tensor::zeros(
+            Shape::new([1, PATCH_TOTAL + 1, EMBED_VECTOR_LEN]),
+            device,
+        ));
         // let encoder_layer = TransformerEncoderLayer
         let transformer_encoder =
             TransformerEncoderConfig::new(EMBED_VECTOR_LEN, DIM_FEEDFORWARD, HEAD, LAYERS)
                 .with_norm_first(true);
         let mlp_head = LinearConfig::new(EMBED_VECTOR_LEN, 10);
 
-        todo!()
+        Vit {
+            patch_embedding: patch_embedding.init(device),
+            cls,
+            position_embedding,
+            transformer_encoder: transformer_encoder.init(device),
+            mlp_head: mlp_head.init(device),
+        }
     }
 }
 
 #[derive(Module, Debug)]
 pub struct Vit<B: Backend> {
     patch_embedding: Linear<B>,
-    // cls: Parameter
-    // position_embedding:
-    encoder_layer: TransformerEncoderLayer<B>,
+    cls: Param<Tensor<B, 3>>,
+    position_embedding: Param<Tensor<B, 3>>,
     transformer_encoder: TransformerEncoder<B>,
     mlp_head: Linear<B>,
 }
@@ -75,6 +109,7 @@ impl<B: Backend> Vit<B> {
         // 2つ次元が減るので，5から3次元になる
         let x: Tensor<B, 3> = x.flatten(2, 4);
         let x = self.patch_embedding.forward(x);
+        let cls_token = repeat_interleave::<B, 3, 4>(self.cls.clone().t(), x.shape().dims[0], 0);
 
         todo!()
     }
